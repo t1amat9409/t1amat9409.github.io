7ffcc3c99ef20b75ce0eeb10857d4f1e715a903e
CodeZAR <9409promos@gmail.com>
2018-04-25 12:26:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 (function(window){
+  const {React,ReactDOM} = window;
+  alert("Works");
+  const App = (props) => {
+    return(
+      <h1>Hello App</h1>
+    )
+  }
+  
+  ReactDOM.render(, document.querySelector("#app"));
 })(window)
